a498127d77c3109e4c96d1d4e811f197c7230eeb
jooam <lord0325@naver.com>
2022-04-13 00:06:43 +0900

    programmers lv2 12973 이 쉬운 걸 생각 못했네 - 스택 문제를 푼지 오래돼서 생각이 안 났다. - 주기적으로 기름칠을 해주자 - 문자를 split하고 map 메소드를 썼더니 효율성을 통과 못함

--- programmers/level2/12973.js
+++ programmers/level2/12973.js
@@ -1,9 +1,22 @@
 function solution(s) {
   const stack = [];
+  let top = -1;
 
-  return _s.length ? 0 : 1;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === stack[top]) {
+      stack.pop();
+      top--;
+    } else {
+      stack.push(s[i]);
+      top++;
+    }
+  }
+
+  console.log(stack);
+
+  return stack.length ? 0 : 1;
 }
 
-const s = 'baabaac';
+const s = 'baabaa';
 
 solution(s);
