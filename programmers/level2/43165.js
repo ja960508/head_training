34413f69b3396dbdc3df860526e584e5050187b8
jooam <lord0325@naver.com>
2022-04-17 15:41:34 +0900

    [feat]: 프로그래머스 lv2 43165
    - DFS, dp로 풀이 가능
    - DFS가 더 효율성이 좋음
    - 반복문과 메소드의 차이인가?

--- programmers/level2/43165.js
+++ programmers/level2/43165.js
@@ -1,10 +1,5 @@
 function solution(numbers, target) {
-  let answer = 0;
-  const dp = [];
-
-  for (let i = 0; i <= numbers.length; i++) {
-    dp.push([]);
-  }
+  const dp = Array.from(Array(numbers.length + 1), () => new Array());
 
   dp[0] = [0];
 
@@ -18,7 +13,44 @@ function solution(numbers, target) {
   return dp[numbers.length].filter((d) => d === target).length;
 }
 
-const numbers = [1, 1, 1, 1, 1];
-const target = 3;
+// function solution(numbers, target) {
+//   function DFS(value, depth) {
+//     if (depth === numbers.length) {
+//       if (value === target) return 1;
+//       else return 0;
+//     } else {
+//       return (
+//         DFS(value + numbers[depth], depth + 1) +
+//         DFS(value - numbers[depth], depth + 1)
+//       );
+//     }
+//   }
+
+//   return DFS(0, 0);
+// }
+
+// function solution(numbers, target) {
+//   let answer = 0;
+//   const willVisit = [[0, 0]];
+
+//   while (willVisit.length > 0) {
+//     const [value, depth] = willVisit.shift();
+
+//     if (depth === numbers.length && value === target) {
+//       answer++;
+//       continue;
+//     }
+
+//     if (depth < numbers.length) {
+//       willVisit.push([value + numbers[depth], depth + 1]);
+//       willVisit.push([value - numbers[depth], depth + 1]);
+//     }
+//   }
+
+//   return answer;
+// }
+
+const numbers = [4, 1, 2, 1];
+const target = 4;
 
 solution(numbers, target);
