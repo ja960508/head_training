536ed8b64bd6fc7b5e733f7bb76d97b0b79fa16a
jooam <lord0325@naver.com>
2022-09-13 20:55:04 +0900

    feat: 프로그래머스 12941 최소값 만들기, 리듀스 쓸걸

---
+++ rehabilitation/programmers/12941.js
@@ -0,0 +1,17 @@
+function solution(A, B) {
+  let answer = 0;
+
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+
+  for (let i = 0; i < A.length; i++) {
+    answer += A[i] * B[i];
+  }
+
+  return answer;
+}
+
+const A = [1, 4, 2];
+const B = [5, 4, 4];
+
+solution(A, B);
